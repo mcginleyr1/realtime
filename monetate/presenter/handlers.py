054d286233e0ad4555321de46741bf3ab7d701a4
Eric Snyder <esnyder@monetate.com>
2011-12-14 19:30:28 -0500

    added sample websocket handler

--- monetate/presenter/handlers.py
+++ monetate/presenter/handlers.py
@@ -1,1 +1,46 @@
-__author__ = 'esnyder'
+import datetime
+
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('index.html', title='Foo')
+
+
+class ChartHandler(tornado.websocket.WebSocketHandler):
+    def _send_chart_data_once(self):
+        """
+        Send the chart data in a message.
+        The handler will convert the dict into JSON.
+
+        As of this writing, WebSockets is disabled in FF 8
+        and tornado websockets don't work with Chrome 16.
+        Safari 5.1.2 works though, woo hoo!
+        """
+        
+        if not self.stream.closed():
+            self.write_message({'foo': 'bar'})
+
+    def _send_chart_data_and_requeue(self):
+        if self.stream.closed():
+            pass
+        else:
+            self._send_chart_data_once()
+
+            tornado.ioloop.IOLoop.instance().add_timeout(
+                datetime.timedelta(seconds=3),
+                self.async_callback(self._send_chart_data_and_requeue)
+            )
+
+    def open(self, *args, **kwargs):
+        print "WebSocket opened"
+
+    def on_message(self, message):
+        tornado.ioloop.IOLoop.instance().add_callback(
+            self.async_callback(self._send_chart_data_and_requeue)
+        )
+
+    def on_close(self):
+        print "WebSocket closed"
--- monetate/presenter/main.py
+++ monetate/presenter/main.py
@@ -2,19 +2,18 @@ import os.path
 import sys
 
 import tornado.ioloop
-import tornado.template
 import tornado.web
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from monetate import PROJECT_ROOT
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self, *args, **kwargs):
-        self.render('index.html', title='Foo')
+from monetate.presenter import handlers
 
 application = tornado.web.Application(
-    handlers=[(r"/", MainHandler),],
+    handlers=[
+        (r"/", handlers.MainHandler),
+        (r"/wstest", handlers.ChartHandler)
+    ],
     template_path=os.path.join(PROJECT_ROOT, 'html'),
     # debug=True adds autoreload and recompiles templates on every request
     debug=True
@@ -23,4 +22,3 @@ application = tornado.web.Application(
 if __name__ == "__main__":
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
-
