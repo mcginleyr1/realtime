7e6259460fb8ac7700bb16495c52718c5bc6e6dc
Rob McGinley <rmcginley@monetate.com>
2011-12-15 08:30:48 -0500

    Renamed presenter to services

--- monetate/services/main.py
+++ monetate/services/main.py
@@ -7,7 +7,7 @@ import tornado.web
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from monetate import PROJECT_ROOT
-from monetate.presenter import handlers
+from monetate.services import handlers
 
 application = tornado.web.Application(
     handlers=[
