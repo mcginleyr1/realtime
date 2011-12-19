37d61bddbb33a34840cf510a52f005d7c3dbe4d6
Rob McGinley <rmcginley@monetate.com>
2011-12-19 14:53:39 -0500

    Updates to keys for redis.

--- monetate/keys.py
+++ monetate/keys.py
@@ -16,8 +16,38 @@ campaign_metrics = set([
 
 campaign_key_patterns = {metric: '%(campaign_id)s/' + metric for metric in campaign_metrics}
 
+#Redis Keys
 def get_campaign_key(campaign_id, metric):
     return campaign_key_patterns[metric] % {'campaign_id': campaign_id}
 
-#Redis Keyss    
-ORDER_VALUE = '%s/order_value/'
+def get_add_to_cart_key(account, campaign, group):
+    add_to_cart = '%s/add_to_cart/%s/%s'
+    return add_to_cart % (account, campaign, group)
+
+def get_bounce_rate_key(account, campaign, group):
+    bounce_rate = "%s/bounce_rate/%s/%s"
+    return bounce_rate  % (account, campaign, group)
+
+def get_total_sales_key(account, campaign, group):
+    total_sales = '%s/control_total_sales/%s/%s'
+    return total_sales % (account, campaign, group)
+
+def get_conversion_key(account, campaign, group):
+    conversion = '%s/conversion/%s/%s'
+    return conversion % (account, campaign, group)
+
+def get_group_key(account, campaign, group):
+    group_key = '%s/experiment_group/%s/%s'
+    return group_key % (account, campaign, group)
+
+def get_order_value_key(account, campaign, group):
+    order_value = '%s/order_value/%s/%s'
+    return order_value % (account, campaign, group)
+
+def get_session_value_key(account, campaign, group):
+    session_value = '%s/session_values/%s/%s'
+    return session_value % (account, campaign, group)
+
+def get_new_visitors_key(account):
+    new_visitors = '%s/new_visitors'
+    return new_visitors % account
--- monetate/services/collector/recorder.py
+++ monetate/services/collector/recorder.py
@@ -1,4 +1,4 @@
-import datetime
+import decimal
 
 import tornado.ioloop
 import tornado.escape
@@ -7,6 +7,7 @@ import tornado.websocket
 
 from monetate.config import settings
 from monetate import keys
+from monetate.config.database import Redis
 
 GIF = ('GIF89a'
     '\x01\x00\x01\x00\x80\xff\x00\xff\xff\xff\x00\x00\x00\x2c\x00\x00'
@@ -17,34 +18,30 @@ KEYS = ['purchase_total', 'add_to_cart']
 class Recorder(tornado.web.RequestHandler):
 
     def __init__(self, *request, **kwargs):
+        redis = Redis()
+        self.redis = redis.client
         super(Recorder,self).__init__(*request,**kwargs)
         self.dispatch_table = {
 
         }
 
     def get(self, *args, **kwargs):
-        purchase_total = self.get_argument('purchase_total')
-        data = tornado.escape.json_decode(qs)
-        tornado.ioloop.IOLoop.instance().add_callback(
-            self.async_callback(self.record, data)
-        )
+        account = self.get_argument('act')
+        group = self.get_argument('grp')
+        purchase_total = self.get_argument('pt', None)
+        if purchase_total:
+            tornado.ioloop.IOLoop.instance().add_callback(
+                self.async_callback(self.request_purchase_total_update, account, group, purchase_total)
+            )
         self.set_header("content-type","image/gif")
         self.write(GIF)
         self.flush()
 
-    def record(self, data):
-        """
-        Records the data.
-        """
-        c = brukva.Client()
-        c.connect()
-        for key in KEYS:
-            value = self.get_argument(key, None)
-            if key == 'purchase_total':
-                update_purchase_total(value)
 
+    def request_purchase_total_update(self, account, group, value):
+        key = keys.get_order_value_key(account, group)
+        self.redis.hincrby(key, 'value', float(value))
+        self.redis.hincrby(key, 'count', 1)
 
-    def update_purchase_total(self, value):
-        brukva.
 
 
