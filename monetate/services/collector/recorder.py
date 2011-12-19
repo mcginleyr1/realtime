7e1f677e7ed7b7baa3c07cc97d6a624cc3ece7ad
Rob McGinley <rmcginley@monetate.com>
2011-12-19 15:31:21 -0500

    Start recording more into redis.

--- monetate/services/collector/recorder.py
+++ monetate/services/collector/recorder.py
@@ -26,22 +26,60 @@ class Recorder(tornado.web.RequestHandler):
         }
 
     def get(self, *args, **kwargs):
-        account = self.get_argument('act')
-        group = self.get_argument('grp')
-        purchase_total = self.get_argument('pt', None)
-        if purchase_total:
-            tornado.ioloop.IOLoop.instance().add_callback(
-                self.async_callback(self.request_purchase_total_update, account, group, purchase_total)
-            )
+        io = tornadio.ioloop.IOLoop.instance()
+        account = self.get_argument('act', None)
+        group = self.get_argument('grp', None)
+        purchase_total = self.get_argument('pch', None)
+        cid = self.get_argument('cid', None)
+        add_to_cart = self.get_argument('atc', None)
+        new_customer = self.get_argument('nc', None)
+        io.add_callback(self.async_callback(self.purchase_total_update, account, cid, group, purchase_total))
+        io.add_callback(self.async_callback(self.add_to_cart_update, cid, account, group, add_to_cart))
+        io.add_callback(self.async_callback(self.add_to_total_sales, cid, account, group, purchase_total))
+        io.add_callback(self.async_callback(self.add_to_group, cid, account, group))
+        io.add_callback(self.async_callback(self.update_conversion, cid, account, group, purchase_total))
+        io.add_callback(self.async_callback(self.add_to_new_visitors, cid, account, group, new_customer))
+        io.add_callback(self.async_callback(self.update_session_value, cid, account, group, purchase_total))
         self.set_header("content-type","image/gif")
         self.write(GIF)
         self.flush()
 
 
-    def request_purchase_total_update(self, account, group, value):
-        key = keys.get_order_value_key(account, group)
-        self.redis.hincrby(key, 'value', float(value))
+    def purchase_total_update(self, account, cid, group, value):
+        if value:
+            key = keys.get_order_value_key(account, cid, group)
+            self.redis.hincrby(key, 'value', float(value))
+            self.redis.hincrby(key, 'count', 1)
+
+    def add_to_cart_update(self, account, cid, group, value):
+        if value:
+            key = keys.get_add_to_cart_key(account, cid, group)
+            self.redis.incr(key)
+
+    def add_to_total_sales(self, account, cid, group, value):
+        if value:
+            key = keys.get_total_sales_key(account, cid, group)
+            self.redis.incrby(key, value)
+
+    def add_to_group(self, account, cid, group):
+        key = keys.get_group_key(account,cid, group)
+        self.redis.incr(key)
+
+    def update_conversion(self, account, cid, group, value):
+        key = keys.get_group_key(account, cid, group)
+        if value:
+            self.redis.hincrby(key, 'value', 1)
         self.redis.hincrby(key, 'count', 1)
 
+    def add_to_new_visitors(self, account, value):
+        if value:
+            key = keys.get_new_visitors_key(account)
+            self.redis.incr(key)
+
+    def update_session_value(self, account, cid, group, value):
+        key = keys.get_session_value_key(account, cid, group)
+        if value:
+            self.redis.hincrby(key, 'value', float(value))
+        self.redis.hincrby(key, 'count', 1)
 
 
