36600b89aca98c5fb01c516927c5f286694018c2
Eric Snyder <esnyder@monetate.com>
2011-12-19 10:30:43 -0500

    Moved static files under one dir so tornado can serve them.

--- monetate/config/settings.py
+++ monetate/config/settings.py
@@ -1,10 +1,13 @@
 import os.path
 
 PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
+STATIC_PATH = os.path.join(PROJECT_ROOT, 'static')
+TEMPLATE_PATH = os.path.join(PROJECT_ROOT, 'templates')
 
 DEFAULT_DSN = 'postgresql+psycopg2://user:password@host:port/dbname'
 
 COLLECTOR_PORT = 8888
 UI_PORT = 8889
 
-PIXEL = open('../../images/pixel.gif')
+# Made absolute so that you can start tornado from any dir.
+PIXEL = open(os.path.join(STATIC_PATH, 'images', 'pixel.gif'))
