c19b253f9c7a34c708c5ff05add89394b13c980d
Eric Snyder <esnyder@monetate.com>
2011-12-20 15:28:52 -0500

    added pause

--- monetate/services/ui/handlers.py
+++ monetate/services/ui/handlers.py
@@ -46,6 +46,8 @@ class RedisWebSocketHandler(tornado.websocket.WebSocketHandler):
 class CampaignMetricDataHandler(RedisWebSocketHandler):
     # TODO: Use a separate IOLoop for the WebSocket stuff?
 
+    paused = False
+
     def _get_add_to_cart_values(self,
         add_to_cart_count_control, add_to_cart_count_experiment,
         group_control,  group_experiment):
@@ -114,51 +116,57 @@ class CampaignMetricDataHandler(RedisWebSocketHandler):
         If the WebSocket stream is still open then send the metric data
         and queue this method up again in the event loop.
         """
-        multi_results = self.redis_client.mget(
-            [
-                redis_keys.get_group_key(account_id, campaign_id, redis_keys.GROUP_CONTROL),
-                redis_keys.get_group_key(account_id, campaign_id, redis_keys.GROUP_EXPERIMENT),
-                redis_keys.get_total_sales_key(account_id, campaign_id, redis_keys.GROUP_CONTROL),
-                redis_keys.get_total_sales_key(account_id, campaign_id, redis_keys.GROUP_EXPERIMENT),
-                redis_keys.get_add_to_cart_key(account_id, campaign_id, redis_keys.GROUP_CONTROL),
-                redis_keys.get_add_to_cart_key(account_id, campaign_id, redis_keys.GROUP_EXPERIMENT)
-            ]
-        )
-
-        rps_control, rps_experiment = self._get_session_value_values(
-            account_id, campaign_id)
-
-        aov_control, aov_experiment = self._get_order_value_values(
-            account_id, campaign_id)
-
-        group_control = multi_results[0]
-        group_experiment = multi_results[1]
-
-        add_to_cart_control, add_to_cart_experiment = self._get_add_to_cart_values(
-            multi_results[4], multi_results[5], group_control, group_experiment)
-
-        data = {
-            'group_control': group_control,
-            'group_experiment': group_experiment,
-            'total_sales_control': multi_results[2],
-            'total_sales_experiment': multi_results[3],
-            'order_value_control': aov_control,
-            'order_value_experiment': aov_experiment,
-            'add_to_cart_control': add_to_cart_control,
-            'add_to_cart_experiment': add_to_cart_experiment,
-            'session_control': rps_control,
-            'session_experiment': rps_experiment
-        }
-
-        if self.stream.closed():
-            pass
-        else:
-            self.write_message(data)
-
+        def _requeue():
             tornado.ioloop.IOLoop.instance().add_timeout(
                 datetime.timedelta(seconds=2),
                 self.async_callback(self._send_metric_data_and_requeue, account_id, campaign_id)
             )
+
+        if self.paused:
+            _requeue()
+        else:
+            multi_results = self.redis_client.mget(
+                [
+                    redis_keys.get_group_key(account_id, campaign_id, redis_keys.GROUP_CONTROL),
+                    redis_keys.get_group_key(account_id, campaign_id, redis_keys.GROUP_EXPERIMENT),
+                    redis_keys.get_total_sales_key(account_id, campaign_id, redis_keys.GROUP_CONTROL),
+                    redis_keys.get_total_sales_key(account_id, campaign_id, redis_keys.GROUP_EXPERIMENT),
+                    redis_keys.get_add_to_cart_key(account_id, campaign_id, redis_keys.GROUP_CONTROL),
+                    redis_keys.get_add_to_cart_key(account_id, campaign_id, redis_keys.GROUP_EXPERIMENT)
+                ]
+            )
+
+            rps_control, rps_experiment = self._get_session_value_values(
+                account_id, campaign_id)
+
+            aov_control, aov_experiment = self._get_order_value_values(
+                account_id, campaign_id)
+
+            group_control = multi_results[0]
+            group_experiment = multi_results[1]
+
+            add_to_cart_control, add_to_cart_experiment = self._get_add_to_cart_values(
+                multi_results[4], multi_results[5], group_control, group_experiment)
+
+            data = {
+                'group_control': group_control,
+                'group_experiment': group_experiment,
+                'total_sales_control': multi_results[2],
+                'total_sales_experiment': multi_results[3],
+                'order_value_control': aov_control,
+                'order_value_experiment': aov_experiment,
+                'add_to_cart_control': add_to_cart_control,
+                'add_to_cart_experiment': add_to_cart_experiment,
+                'session_control': rps_control,
+                'session_experiment': rps_experiment
+            }
+
+            if self.stream.closed():
+                pass
+            else:
+                self.write_message(data)
+
+                _requeue()
 
 
     def on_message(self, message):
@@ -167,10 +175,13 @@ class CampaignMetricDataHandler(RedisWebSocketHandler):
         We don't care what the message is, it's just a signal
         that we can start repeatedly sending the client metric data.
         """
-        account_id, campaign_id = message.split('/')
-        account_id = int(account_id)
-        campaign_id = int(campaign_id)
+        if message == 'pause':
+            self.paused = (not self.paused)
+        else:
+            account_id, campaign_id = message.split('/')
+            account_id = int(account_id)
+            campaign_id = int(campaign_id)
 
-        tornado.ioloop.IOLoop.instance().add_callback(
-            self.async_callback(self._send_metric_data_and_requeue, account_id, campaign_id)
-        )
+            tornado.ioloop.IOLoop.instance().add_callback(
+                self.async_callback(self._send_metric_data_and_requeue, account_id, campaign_id)
+            )
