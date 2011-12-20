2a86acc3f5bbef37a7334c5b12cac77967e25fd4
Rob McGinley <rmcginley@monetate.com>
2011-12-20 13:18:45 -0500

    No longer a replay so much as a random loader.

--- tools/replay.py
+++ tools/replay.py
@@ -3,12 +3,12 @@ from contextlib import closing
 
 import boto
 import boto.s3.key
-import bz2
+import decimal
 import optparse
 import re
 import simplejson
-import tempfile
 import urllib2
+import random
 
 LOG_MATCHER = re.compile(
     "(?P<logdir>\w+)\D"
@@ -20,16 +20,11 @@ LOG_MATCHER = re.compile(
     "(?P<timestamp>\w+)"
     "\.log\.bz2\Z")
 
-def parse_s3_uri(url):
-    """
-    Parse s3 url into (bucket, prefix) tuple.
-    Supported urls:
-        s3://bucket/prefix
-        bucket:prefix
-    """
-    if url.startswith("s3://"):
-        return url[5:].split('/', 1)
-    return url.split(':', 1)
+ACCT_CPG_MAP = {
+    '129': ['1345', '2382', '2992'],
+    '52': ['18494', '8329', '2938']
+}
+
 
 def get_track_data(line):
     """
@@ -38,27 +33,9 @@ def get_track_data(line):
     """
     return simplejson.loads(line.split(' ', 3)[3])
 
-
-def query_s3_track_set(uri):
-    """
-    Retrieve all files in an particular s3 buckets with a
-    prefix matching the one in our uri.
-    """
-    urls = set()
-    (bucket_name, key_prefix) = parse_s3_uri(uri)
-    s3 = boto.connect_s3() # New connection for each s3 request
-    bucket = s3.get_bucket(bucket_name, validate=False) # Do not list all keys in bucket
-    for key in bucket.list(prefix=key_prefix):
-        m = LOG_MATCHER.match(key.name)
-        if m:
-            d = m.groupdict()
-            if d['type'] == 'session':
-                urls.add( "s3://%s/%s" % (bucket_name, key.name) )
-    return urls
-
 def cat_key(line, key, value):
     """ Concat together the new value and the existing line """
-    if value:
+    if value or value == '0':
         if line:
             line = "%s&%s=%s" % (line, key, value)
         else:
@@ -77,53 +54,33 @@ def create_line(account, add_to_cart, new_customer, grp, purchase_value=None, ci
     return line
 
 
-def format_request_data(data):
+def format_request_data():
     """
     Returns a string matching how our request should look
     """
-    account = data['visitor']['account_id']
-    add_to_cart = data['visit']['has_add_cart']
-    new_customer = data['visit']['new_customer']
-    purchases = data['session']['purchase']
-    offers = data['session']['offer']
-    grp = '0'
-    if not purchases:
-        line = create_line(account, add_to_cart, new_customer, grp)
+    account = ACCT_CPG_MAP.keys()[random.randint(0,1)]
+    cid = ACCT_CPG_MAP[account][random.randint(0, 2)]
+    add_to_cart = str(random.randint(0, 1))
+    new_customer = str(random.randint(0, 1))
+    grp = random.randint(0,1)
+    for i in range(0, 100):
+        purchases = random.randint(0, 1)
+        purchase_value = None
+        if purchases:
+            purchase_value = round(decimal.Decimal(str(random.uniform(0, 200))), 2)
+        line = create_line(account, add_to_cart, new_customer, grp, purchase_value, cid)
         yield '?%s' % line
-    for purchase in purchases:
-        purchase_value = purchase.get('value', None)
-        if not offers:
-            line = create_line(account, 
-                    add_to_cart, new_customer, grp, purchase_value)
-        for offer in offers:
-            grp = offer['grp']
-            cid = offer['campaign_id']
-            line = create_line(account, add_to_cart, new_customer, grp, purchase_value, cid)
-            yield '?%s' % line
    
-def main(s3_uri, ip):
-    urls = query_s3_track_set(s3_uri)
-    for url in urls:
-        (bucket_name, key_name) = parse_s3_uri(url)
-        s3 = boto.connect_s3()
-        bucket = s3.get_bucket(bucket_name, validate=False)
-        key = boto.s3.key.Key(bucket, key_name)
-        with closing(tempfile.NamedTemporaryFile()) as tmp:
-            key.get_file(tmp)
-            tmp.seek(0)
-            with closing(bz2.BZ2File(tmp.name)) as b:
-                line = b.readline()
-                while line:
-                    data = get_track_data(line)
-                    for query_str in format_request_data(data):
-                        record_url = "http://%s:8899/intelligence/%s" % (ip, query_str)
-                        urllib2.urlopen(record_url)
-                    line = b.readline()
+def main(ip):
+    while True:
+        for query_str in format_request_data():
+            record_url = "http://%s:8899/intelligence/%s" % (ip, query_str)
+            print record_url
+            urllib2.urlopen(record_url)
 
 
 if __name__ == "__main__":
     parser = optparse.OptionParser()
-    parser.add_option('--uri', type='str', dest='uri', help="s3_uri to look for logs in.")
     parser.add_option('--ip', type='str', dest='ip', help="IP to hit with query")
     options, args = parser.parse_args()
-    main(options.uri, options.ip)
+    main(options.ip)
